cc62cfd6b5c541600d07bd171b9ef4b6c8e5bedc
xiabai <xiabai@redhat.com>
2014-04-24 14:25:24 +0800

    chage ,model

--- src/main/java/com/redhat/hss/bugzilla/model/Bug.java
+++ src/main/java/com/redhat/hss/bugzilla/model/Bug.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package model;
+package com.redhat.hss.bugzilla.model;
 
 import com.google.common.base.Optional;
 
